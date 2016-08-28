9f20016cfd0ef738f45428ddbca2c068d94b58f2
weiquling <weiquling@iiiiiMacBook-Pro.local>
2016-08-28 18:25:11 +0800

    解决卡顿问题，解决点击状态栏后ScrollView不能回滚置顶问题

---
+++ MyOne/工具/TopWindow.swift
@@ -0,0 +1,21 @@
+//
+//  TopWindow.swift
+//  MyOne
+//
+//  Created by 韦曲凌 on 16/8/28.
+//  Copyright © 2016年 Wake GetZero. All rights reserved.
+//
+
+import UIKit
+
+class TopWindow: UIWindow {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ MyOne/文章/View/ArticleViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  ArticleViewCell.swift
+//  MyOne
+//
+//  Created by 韦曲凌 on 16/8/28.
+//  Copyright © 2016年 Wake GetZero. All rights reserved.
+//
+
+import UIKit
+
+class ArticleViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
